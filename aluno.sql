0e51bd0dd5f1728c195b8fd9b4c657ceb4cb7730
juliabeatriz09 <159266648+juliabeatriz09@users.noreply.github.com>
2025-06-11 08:41:36 -0300

    Add files via upload

---
+++ aluno.sql
@@ -0,0 +1,18 @@
+--- 11-06-2025 08:40:39 SQLite
+CREATE table if not EXISTS aluno(
+  CGM integer PRIMARY KEY AUTOINCREMENT NOT null UNIQUE,
+  nome varchar (50) not NULL,
+  endereço text,
+  periodo varchar (5) not null ,
+  cpf varchar (11) not NULL UNIQUE,
+  matricula date NOT NULL,
+  nascimento date not NULL,
+  rg varchar (9) NOT NULL unique ,
+  genero text,
+  telefone varchar (15),
+  turma varchar (3) not NULL,
+  responsavel
+  );
+  
+
+
