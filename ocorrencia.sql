9a26b8cf65c88bb3cb6f2bef4b731c50125ef5bc
bruno duarte <159266616+Brunoduartemarcanzoni@users.noreply.github.com>
2025-06-11 16:45:30 -0300

    Update ocorrencia.sql

--- ocorrencia.sql
+++ ocorrencia.sql
@@ -1,6 +1,5 @@
---- 11-06-2025 10:59:49 SQLite
 CREATE table if not EXISTS ocorrencia(
-id_sequencial_ocorrencia INTEGER PRIMARY key,
+id_sequencial_ocorrencia INTEGER PRIMARY key AUTOINCREMENT,
 fato_id varchar (50), 
 cpf_funcionario VARCHAR,
 data DATE(8),
@@ -10,4 +9,13 @@ obs VARCHAR(500),
 turma VARCHAR(3)
 );
 
-
+INSERT INTO ocorrencia (fato_id, cpf_funcionario, data, horario, cgm_aluno, obs, turma)
+VALUES (
+  '1581561',
+  '444.555.666-74',
+  '2025-04-24',
+  '09:55',
+  '15648416',
+  'aluno demorou pra entrar pra sala',
+  '3c'
+  );
