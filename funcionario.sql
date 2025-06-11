6faa9bec7b0528aaefe02eeebb6f9c096359ecde
juliabeatriz09 <159266648+juliabeatriz09@users.noreply.github.com>
2025-06-11 08:27:39 -0300

    Update funcionario.sql

--- funcionario.sql
+++ funcionario.sql
@@ -1,15 +1,13 @@
 --- 11-06-2025 07:57:45 SQLite
 CREATE table if not EXISTS funcionario(
-  CHAR nome  
-  CHAR CPF 
-  CHAR CARGA HORARIA  
-  CHAR ID FUNÇÃO 
-  CHAR ENDEREÇO 
-  CHAR TURNO  
-  CHAR ESTADO  
-  CHAR GENERO  
-  INT TELEFONE
-  VARCHAR DATA DE ADIMIÇÃO
+  nome VARCHAR(100) NOT NULL,
+  cpf VARCHAR(14) NOT NULL UNIQUE,
+  carga horaria VARCHAR,
+  id função INTEGER PRIMARY key AUTOINCREMENT,
+  endereço TEXT,
+  turno VARCHAR, 
+  estado VARCHAR, 
+  genero VARCHAR,  
+  telefone VARCHAR(15),
+  data_adimição VARCHAR 
   );
-
-
