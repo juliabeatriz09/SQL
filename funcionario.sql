fe1193c0aa6e892b25c86b6974686b2c36d8760e
bruno duarte <159266616+Brunoduartemarcanzoni@users.noreply.github.com>
2025-06-12 08:11:39 -0300

    Update funcionario.sql

--- funcionario.sql
+++ funcionario.sql
@@ -1,13 +1,25 @@
---- 11-06-2025 07:57:45 SQLite
 CREATE table if not EXISTS funcionario(
   nome VARCHAR(100) NOT NULL,
   cpf VARCHAR(14) NOT NULL UNIQUE PRIMARY key,
-  carga horaria VARCHAR,
-  id função INTEGER,
+  carga_horaria VARCHAR,
+  id_função VARCHAR,
   endereço TEXT,
   turno VARCHAR, 
   estado VARCHAR, 
   genero VARCHAR,  
   telefone VARCHAR(15),
-  data_adimição VARCHAR 
+  data_adimição DATE 
+  );
+  
+  INSERT INTO funcionario (nome, cpf, carga_horaria, endereço, turno, estado, genero, telefone, data_adimição)
+VALUES (
+  'Patrcia Janaina',
+  '777.888.999-66',
+  '8h',
+  'Av das cataratas, 345',
+  'matutino',
+  'Solteiro',
+  'feminino',
+  '(45) 99956-230',
+  '2020-06-03'
   );
