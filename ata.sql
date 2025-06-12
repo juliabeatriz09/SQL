2382f23aabc8f6807644706b2c20b059c6bac0cb
bruno duarte <159266616+Brunoduartemarcanzoni@users.noreply.github.com>
2025-06-12 07:46:38 -0300

    Update ata.sql

--- ata.sql
+++ ata.sql
@@ -1,5 +1,5 @@
 CREATE TABLE if not EXISTS ata(
-  id ata integer primary KEY ,
+  id_ata integer primary KEY AUTOINCREMENT,
   aluno varchar (50) not null,
   dia date not null,
   assunto varchar (500) not null,
@@ -10,4 +10,18 @@ CREATE TABLE if not EXISTS ata(
   turma varchar (3) not null,
   prof varchar (20),
   fato varchar (20) not null
+  );
+  
+  INSERT INTO ata (aluno, dia, assunto, monitor, conteudo, encaminhamento, cgm, turma, prof, fato)
+VALUES (
+  'Bruno Duarte',
+  '2025-02-03',
+  'desacato a funcionario publico',
+  'Cabo Ramalho',
+  'Aluno faltou com respeito com as merenderas',
+  'diretoria',
+  '1542369',
+  '3c',
+  'daffara',
+  'menos dois - Gravissimo'
   );
