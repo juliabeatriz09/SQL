e7c63a0282977c18a06771601f455383ba051914
bruno duarte <159266616+Brunoduartemarcanzoni@users.noreply.github.com>
2025-06-12 08:02:19 -0300

    Update frequencia.sql

--- frequencia.sql
+++ frequencia.sql
@@ -1,8 +1,18 @@
 CREATE table if not EXISTS frequencia (
   frequencia varchar (10),
-  cgm integer PRIMARY key AUTOINCREMENT,
+  cgm integer PRIMARY key,
   dia date,
-  quant aulas int,
+  quant_aulas int,
   ausencias int,
   turma varchar (4)
-  )
+  );
+  
+  INSERT INTO frequencia (frequencia, cgm, dia, quant_aulas, ausencias, turma)
+VALUES (
+  'em risco',
+  '78456321',
+  '2025-10-25',
+  '7',
+  '4',
+  '3c'
+  );
