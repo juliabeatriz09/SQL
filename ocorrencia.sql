a1d2ff46c0837219e19614efc9cc488a55282d83
talitavitoriapereira <159267033+talitavitoriapereira@users.noreply.github.com>
2025-06-12 09:16:14 -0300

    Update ocorrencia.sql

--- ocorrencia.sql
+++ ocorrencia.sql
@@ -1,12 +1,12 @@
 CREATE table if not EXISTS ocorrencia(
 id_sequencial_ocorrencia INTEGER PRIMARY key AUTOINCREMENT,
-fato_id varchar (50), 
-cpf_funcionario VARCHAR,
-data DATE(8),
+fato_id varchar (50)NOT NULL, 
+cpf_funcionario VARCHAR NOT NULL UNIQUE,
+data DATE(8)NOT NULL,
 horario VARCHAR(4),
-cgm_aluno VARCHAR(9),
+cgm_aluno VARCHAR(9)NOT NULL UNIQUE ,
 obs VARCHAR(500),
-turma VARCHAR(3)
+turma VARCHAR(3) NOT NULL 
 );
 
 INSERT INTO ocorrencia (fato_id, cpf_funcionario, data, horario, cgm_aluno, obs, turma)
