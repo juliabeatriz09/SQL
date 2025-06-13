7a058820448d6be894828ef847320ec08ee51cea
talitavitoriapereira <159267033+talitavitoriapereira@users.noreply.github.com>
2025-06-13 08:31:28 -0300

    Update funcionario.sql

--- funcionario.sql
+++ funcionario.sql
@@ -2,13 +2,14 @@ CREATE table if not EXISTS funcionario(
   nome VARCHAR(100) NOT NULL,
   cpf VARCHAR(14) NOT NULL UNIQUE PRIMARY key,
   carga_horaria VARCHAR NOT NULL,
-  id_função VARCHAR,
   endereço TEXT,
   turno VARCHAR NOT NULL, 
   estado VARCHAR, 
   genero VARCHAR,  
   telefone VARCHAR(15)NOT NULL UNIQUE,
-  data_adimição DATE NOT NULL
+  data_adimição DATE NOT NULL,
+  id  INTEGER,
+  FOREIGN KEY (id) REFERENCES funcao(id)
   );
   
   INSERT INTO funcionario (nome, cpf, carga_horaria, endereço, turno, estado, genero, telefone, data_adimição)
@@ -21,5 +22,4 @@ VALUES (
   'Solteiro',
   'feminino',
   '(45) 99956-230',
-  '2020-06-03'
-  );
+  '2020-06-03
