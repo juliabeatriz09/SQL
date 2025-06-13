94f473e73083e979cb09e3ed3ac457da7c027561
talitavitoriapereira <159267033+talitavitoriapereira@users.noreply.github.com>
2025-06-13 08:19:13 -0300

    Update frequencia.sql

--- frequencia.sql
+++ frequencia.sql
@@ -1,16 +1,15 @@
 CREATE table if not EXISTS frequencia (
-  frequencia varchar (10),
-  cgm integer PRIMARY key NOT NULL UNIQUE,
+  id_frequencia INTEGER PRIMARY KEY AUTOINCREMENT,
   dia date NOT NULL,
   quant_aulas int NOT NULL,
   ausencias int NOT NULL,
-  turma varchar (4) NOT NULL
+  turma varchar (4) NOT NULL,
+  cgm INTEGER,
+  FOREIGN KEY (cgm) REFERENCES aluno(cgm)
   );
   
-  INSERT INTO frequencia (frequencia, cgm, dia, quant_aulas, ausencias, turma)
+  INSERT INTO frequencia ( dia, quant_aulas, ausencias, turma)
 VALUES (
-  'em risco',
-  '78456321',
   '2025-10-25',
   '7',
   '4',
