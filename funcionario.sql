a96390737249742d25dc40841f9b09a4e256990a
bruno duarte <159266616+Brunoduartemarcanzoni@users.noreply.github.com>
2025-06-18 08:41:36 -0300

    Update funcionario.sql

--- funcionario.sql
+++ funcionario.sql
@@ -1,25 +1,25 @@
 CREATE table if not EXISTS funcionario(
   nome VARCHAR(100) NOT NULL,
   cpf VARCHAR(14) NOT NULL UNIQUE PRIMARY key,
-  carga_horaria VARCHAR NOT NULL,
-  endereço TEXT,
-  turno VARCHAR NOT NULL, 
-  estado VARCHAR, 
-  genero VARCHAR,  
+  nascimento date not null,
+  rg varchar (16) not null,
+  genero text,
+  estado_civil text,
+  email text,
+  email_institucional text,
   telefone VARCHAR(15)NOT NULL UNIQUE,
+  telefone_alternativo VARCHAR(15)NOT NULL,
+  cep varchar (9),
+  numero integer,
+  complemento varchar (200),
+  bairro varchar (100),
+  cidade varchar (250),
+  estado varchar (150),
   data_adimição DATE NOT NULL,
+  cargo varchar (500),
+  carga_horaria integer,
+  contrato varchar (80),
+  numero_registro integer,
   id  INTEGER,
   FOREIGN KEY (id) REFERENCES funcao(id)
   );
-  
-  INSERT INTO funcionario (nome, cpf, carga_horaria, endereço, turno, estado, genero, telefone, data_adimição)
-VALUES (
-  'Patrcia Janaina',
-  '777.888.999-66',
-  '8h',
-  'Av das cataratas, 345',
-  'matutino',
-  'Solteiro',
-  'feminino',
-  '(45) 99956-230',
-  '2020-06-03
