3dd08d34701d0bc4a37540f93684616f491968d5
juliabeatriz09 <159266648+juliabeatriz09@users.noreply.github.com>
2025-06-25 20:19:44 -0300

    Update ocorrencia.sql

--- ocorrencia.sql
+++ ocorrencia.sql
@@ -1,17 +1,15 @@
-CREATE table if not EXISTS ocorrencia(
-id_sequencial_ocorrencia INTEGER PRIMARY key AUTOINCREMENT,
-fato_id varchar (50)NOT NULL, 
-cpf_funcionario VARchar,
-FOREIGN key (cpf_funcionario) REFERENCES funcionario (cpf),
-data DATE NOT NULL,
-horario VARCHAR(4),
-cgm_aluno VARCHAR,
-FOREIGN KEY (cgm) REFERENCES aluno (cgm),
-obs VARCHAR(500),
-turma VARCHAR(3) NOT NULL 
-  
+CREATE TABLE IF NOT EXISTS ocorrencia (
+    id_sequencial_ocorrencia INTEGER PRIMARY KEY AUTOINCREMENT,
+    fato_id VARCHAR(50) NOT NULL,
+    cpf_funcionario VARCHAR,
+    data DATE NOT NULL,
+    horario VARCHAR(4),
+    cgm_aluno VARCHAR,
+    obs VARCHAR(500),
+    turma VARCHAR(3) NOT NULL,
+    FOREIGN KEY (cpf_funcionario) REFERENCES funcionario(cpf),
+    FOREIGN KEY (cgm_aluno) REFERENCES aluno(cgm)
 );
-
 INSERT INTO ocorrencia (fato_id, cpf_funcionario, data, horario, cgm_aluno, obs, turma)
 VALUES (
   '1581561',
